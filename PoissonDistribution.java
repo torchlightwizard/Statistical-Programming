b1e30f175e67439f0c6791803df08fc24bd2e4b8
Abbas Haider <73483662+torchlightwizard@users.noreply.github.com>
2024-04-12 10:37:33 +0500

    Update PoissonDistribution name

--- PoissonDistribution.java
+++ PoissonDistribution.java
@@ -1,4 +1,4 @@
-class HelloWorld {
+class PoissonDistribution {
     public static void main(String[] args) {
         int lastKnown = 10;
        int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
